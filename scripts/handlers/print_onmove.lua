19c05c2cf78b87c97edcbbe2ccf5d29b189836c5
nanderv <nanderv@gmail.com>
2017-03-23 15:00:48 +0100

    Small update.

--- scripts/handlers/print_onmove.lua
+++ scripts/handlers/print_onmove.lua
@@ -1,8 +1,7 @@
 return function(entity) 
 	return{name="onMove", func = function(event, dt)
-
-		entity.position.x = entity.position.x + dt * event.data.x
-		entity.position.y = entity.position.y + dt * event.data.y
+			entity.position.x = entity.position.x + dt * event.data.x
+			entity.position.y = entity.position.y + dt * event.data.y
  		end
 	}
 end
